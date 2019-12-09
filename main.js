709c6b7effa44c91b52a4af29b19013470b0ef0e
Sung Paik <sung.paik@codeimmersives.com>
2019-12-09 15:37:53 -0500

    saving progress. up to remove from end

--- main.js
+++ main.js
@@ -1,29 +1,68 @@
-const Node = () => {
-  return {}
+const Node = (value, next = null) => {
+  return {
+    value,
+    next,
+  }
 }
 
 const Lil = () => {
   return {
-    head: '',
+    head: null,
 
     values: function() {
+      let newArr = [];
+      let current = this.head;
 
+      while (current.next) {
+        newArr.push(current.value);
+        current = current.next
+      }
+      newArr.push(current.value)
+
+      return newArr;
     },
 
     addToStart: function(value) {
+      let firstHead = this.head;
 
+      if (this.head === null) {
+        this.head = Node(value);
+      } else {
+        this.head = Node(value);
+        this.head.next = firstHead;
+      }
     },
 
     addToEnd: function(value) {
-    
+      if (this.head === null) {
+        this.head = Node(value);
+      } else if (this.head !== null) {
+        let current = this.head;
+
+        while (current.next) {
+          current = current.next;
+        }
+        current.next = Node(value);
+      }
     },
 
     removeFromStart: function() {
-    
+      let current = this.head;
+      this.head = this.head.next;
+      return current.value;
     },
 
     removeFromEnd: function() {
+      let current = this.head;
+      let prev = current;
+
+      while(current.next) {
+        current = current.next;
+        prev = current;
+      }
     
+      prev.next = null;
+      return current.value;
     },
 
     getAt: function(i) {
@@ -35,7 +74,6 @@ const Lil = () => {
     },
   }
 }
-
 
 module.exports = {
   Lil,
